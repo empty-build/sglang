98fbcb7b205c691bf1eb5310d2069d498d1858d4
wanghanpei <wanghanpei@bytedance.com>
2025-06-11 09:31:41 +0000

    modify static test script

--- examples/runtime/engine/offline_batch_inference_vlm.py
+++ examples/runtime/engine/offline_batch_inference_vlm.py
@@ -5,10 +5,14 @@ python offline_batch_inference_vlm.py --model-path Qwen/Qwen2-VL-7B-Instruct --c
 
 import argparse
 import dataclasses
+import torch
+import time
 
 import sglang as sgl
+import pdb
 from sglang.srt.conversation import chat_templates
 from sglang.srt.server_args import ServerArgs
+from PIL import Image
 
 
 def main(
@@ -19,24 +23,58 @@ def main(
     conv = chat_templates[server_args.chat_template].copy()
     image_token = conv.image_token
 
-    image_url = "https://github.com/sgl-project/sglang/blob/main/test/lang/example_image.png?raw=true"
+    # image_url = "/root/test_img/example_image.png"
+
+    uji_url = "/root/test_img/uji.jpg"
+
+    hoshi_url_large = "/root/test_img/keihann.jpg"
+
+    # img = Image.open(image_url)
+    uji = Image.open(uji_url)
+    hoshi = Image.open(hoshi_url_large)
 
     prompt = f"What's in this image?\n{image_token}"
-
+    prompt_2 = f"What's in this image is a people in this image?\n{image_token}"
+    # pdb.set_trace()
     sampling_params = {
         "temperature": 0.001,
-        "max_new_tokens": 30,
+        "max_new_tokens":30,
     }
+    
 
     output = vlm.generate(
         prompt=prompt,
-        image_data=image_url,
+        image_data=[hoshi],
         sampling_params=sampling_params,
     )
 
+    # output_1 = vlm.generate(
+    #     prompt=prompt_2,
+    #     image_data=[uji],
+    #     sampling_params=sampling_params,
+    # )
+    
     print("===============================")
     print(f"Prompt: {prompt}")
     print(f"Generated text: {output['text']}")
+    print("warmup end")
+    
+    torch.cuda.synchronize()
+    
+    print("======profiling start=====")
+    start_time = time.time()
+    run_num = 1
+    for run_idx in  range(run_num):
+        out_contents = vlm.generate(
+            prompt=prompt,
+            image_data=[uji],
+            sampling_params=sampling_params,
+        )
+    
+    torch.cuda.synchronize()
+    end_time = time.time()
+    during_time = (end_time - start_time) * (1000/run_num)
+    print("prefill time for this image is {} ms \n".format(during_time))
 
     vlm.shutdown()
 
@@ -47,6 +85,6 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     ServerArgs.add_cli_args(parser)
     args = parser.parse_args()
-
+    # pdb.set_trace()
     server_args = ServerArgs.from_cli_args(args)
     main(server_args)
