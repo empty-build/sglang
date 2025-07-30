41e1600bcbd8e8314fc2525ab746043054d773b5
zc8gerard <110657643+zc8gerard@users.noreply.github.com>
2025-07-30 14:55:58 +0800

    fix:fix sgl-kernel build error(ptxas core dump) when using cu12.6 bui… (#209)
    Co-authored-by: zhangcheng.gerard <zhangcheng.gerard@bytedance.com>

--- sgl-kernel/build.sh
+++ sgl-kernel/build.sh
@@ -60,6 +60,7 @@ docker run --rm \
 
    cd /sgl-kernel && \
    ls -la ${PYTHON_ROOT_PATH}/lib/python${PYTHON_VERSION}/site-packages/wheel/ && \
+   bash ./replace_ptxas.sh ${CUDA_VERSION} && \
    PYTHONPATH=${PYTHON_ROOT_PATH}/lib/python${PYTHON_VERSION}/site-packages ${PYTHON_ROOT_PATH}/bin/python -m uv build --wheel -Cbuild-dir=build . --color=always --no-build-isolation && \
    ./rename_wheels.sh
    "
